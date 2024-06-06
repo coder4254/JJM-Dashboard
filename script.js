8e3a7fb77298ffca36c9beb74e04b1acb03449d8
Ayush Agarwal <ayushashu892@gmail.com>
2024-06-06 13:45:12 +0530

    Added Animation remove button

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document.getElementById('toggle-streamlit').addEventListener('click', function() {
-    var container = document.getElementById('streamlit-container');
-    if (container.style.height === '0px' || container.style.height === '') {
-        if (!container.innerHTML) {
-            var iframe = document.createElement('iframe');
-            iframe.id = 'streamlit-iframe';
-            iframe.src = 'https://jjm-chatbot.streamlit.app/';
-            container.appendChild(iframe);
-        }
-        container.style.height = '80vh';  // Adjust the height as needed
-        this.textContent = 'Close Chatbot';
+document
+  .getElementById("toggle-streamlit")
+  .addEventListener("click", function () {
+    var container = document.getElementById("streamlit-container");
+    if (container.style.height === "0px" || container.style.height === "") {
+      if (!container.innerHTML) {
+        var iframe = document.createElement("iframe");
+        iframe.id = "streamlit-iframe";
+        iframe.src = "https://jjm-chatbot.streamlit.app/";
+        container.appendChild(iframe);
+      }
+      container.style.height = "80vh"; // Adjust the height as needed
+      this.textContent = "Close Chatbot";
     } else {
-        container.style.height = '0px';
-        this.textContent = 'Open Chatbot';
+      container.style.height = "0px";
+      this.textContent = "Open Chatbot";
     }
-});
+  });
